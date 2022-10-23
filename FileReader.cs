00170c7e1e0de56891613c779b9eb88ab001bccd
dyingvoid <dyingvoid@gmail.com>
2022-10-23 16:30:31 +0500

    Copied FileReader.cs

---
+++ FileReader.cs
@@ -0,0 +1,43 @@
+﻿namespace Lab8;
+
+class FileReader
+{
+    private string[] _fileContent;
+    private string _filePath;
+
+    public string[] FileContent
+    {
+        get => _fileContent;
+        set => _fileContent = value;
+    }
+
+    public string FilePath
+    {
+        get => _filePath;
+        init
+        {
+            if (File.Exists(value))
+            {
+                _filePath = value;
+            }
+            else
+            {
+                Console.WriteLine("Wrong path. Path is set to empty string.");
+                _filePath = "";
+            }
+        }
+    }
+
+    public FileReader(string filePath)
+    {
+        FilePath = filePath;
+        if(FilePath.Length > 0)
+        {
+            FileContent = File.ReadAllLines(FilePath);
+        }
+        else
+        {
+            FileContent = new string[] { };
+        }
+    }
+}
